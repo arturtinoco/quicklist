83b4c4ae84e8a880eec63e8a03a1b76a079bce4b
arturtinoco <arturguz.tinoco@gmail.com>
2024-10-16 16:43:52 -0300

    documentation comments

--- scripts.js
+++ scripts.js
@@ -1,38 +1,44 @@
-// Gets form elements
+// Get form elements
 const form = document.querySelector("form")
 const user_input = document.getElementById("new-item")
-const input_button = document.querySelector('button[type="submit"]')
+const submit_button = document.querySelector('button[type="submit"]')
 const items_list = document.getElementById("items-list")
 const item = document.getElementsByClassName("item")
 const checkbox = document.querySelector('input[type="checkbox"]')
-const item_name = document.querySelector("span")
-const remove_button = document.querySelector("li > svg")//maybe div > img
+const remove_button = document.querySelector("li > img")
 
-
+// Call submit button events
 form.onsubmit = (event) => {
     event.preventDefault()
 
     addItem(user_input.value)
 
-    input_button.blur()
+    // Unfocus button after use
+    submit_button.blur()
 }
 
+// Insert new item on top of the list
 function addItem(name) {
+    // <li class="item"></li>
     const new_item = document.createElement("li")
-    new_item.setAttribute("class", "item")
+    new_item.classList.add("item")
     
+    // <input type="checkbox">
     const new_checkbox = document.createElement("input")
     new_checkbox.setAttribute("type", "checkbox")
     new_item.append(new_checkbox)
 
+    // <span> user_input.value </span>
     const new_name = document.createElement("span")
     new_name.textContent = name
     new_item.append(new_name)
 
+    // <img src="./..." alt="...">
     const new_remove_img = document.createElement("img")
     new_remove_img.setAttribute("src", "./assets/icons/trash-bin.svg")
     new_remove_img.setAttribute("alt", "Ícone de lata de lixo")
     new_item.append(new_remove_img)
 
+    // Insert new <li class="item"> in <ul role="list" id="items-list"> first position
     items_list.prepend(new_item)
 }
